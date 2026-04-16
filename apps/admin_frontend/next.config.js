535386df87b35b45298a844f939fff1d1eca783f
unknown <akashava50@gmail.com>
2026-04-16 15:47:34 +0530

    Added the clound front url to config

--- apps/admin_frontend/next.config.js
+++ apps/admin_frontend/next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "hellofi-new.s3.ap-south-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "d2mtboq75bgsvj.cloudfront.net", // ← add this
+      },
     ],
   },
   transpilePackages: ["@repo/ui", "@repo/validations"],
--- apps/backend/src/graphql/admin/auth/admin.auth.mutations.ts
+++ apps/backend/src/graphql/admin/auth/admin.auth.mutations.ts
@@ -1,7 +1,7 @@
 export const mutations = `#graphql
    #Admin Login
    adminUserLogin(payload: AdminLoginInput!): AuthResponse!
-   createAdmin(payload: CreateAdminInput!): AuthResponse!
+   # createAdmin(payload: CreateAdminInput!): AuthResponse!
    adminLogout: LogoutResponse!
 
 
