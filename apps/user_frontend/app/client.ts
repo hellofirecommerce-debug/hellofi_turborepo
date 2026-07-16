67da8193aa6ad167cf59b43dc66efbb8d7a47f4b
unknown <akashava50@gmail.com>
2026-07-16 10:43:42 +0530

    Updated the env

--- apps/user_frontend/app/client.ts
+++ apps/user_frontend/app/client.ts
@@ -9,7 +9,7 @@ import { createErrorLink } from "./errorLink";
 
 export function createClient() {
   const httpLink = new HttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:8000/graphql",
+    uri: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000/graphql",
     credentials: "include",
   });
 
